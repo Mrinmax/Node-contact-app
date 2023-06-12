ab1faf3f514a43bb1b03e043952b7a1e3c697263
Mrinal kanti sardar <mrinalkantisardar007@gmail.com>
2023-06-13 02:01:31 +0530

    routes done

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv").config();
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use("/api/contacts", require("./routes/contactRoutes"));
 
 app.listen(port, () => {
   console.log(`Server Running on port ${port}`);
